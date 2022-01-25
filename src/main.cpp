dc8f2e9d34e609bd68d90665a6380c42e6101b95
Jonas Stolle <jonas.stolle@sensirion.com>
2022-01-25 18:08:59 +0100

    Clean up main

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,13 @@
 #include <iostream>
 
 int main() {
-  /*Cap cap(7);
-  cap.sourceImagePath =
-  "/home/jstolle/code/cppbeerxels/data/raw/bottle-cap-2.jpg"; cap.analyze();
-  cv::Mat bottleCapCutOut = cap.getBottleCap();
-  cv::Mat averageColor(300,300, CV_8UC3, cap.getAverageColor());
-  popUpImage(averageColor);
-  */
-  std::filesystem::path referenceImageFilePath  = "/home/jstolle/code/cppbeerxels/data/refernece/656189624.805342.jpg";
-  std::filesystem::path bottleCapDirectoryPath = "/home/jstolle/code/cppbeerxels/data/raw/";
- 
-  CapField field(referenceImageFilePath,bottleCapDirectoryPath);
-  std::cout << "running CapShepherd\n";
-  field.runCapShepherd();
-  std::cout << "processing reference\n";
-  field.processReference();
+  std::filesystem::path referenceImageFilePath =
+      "/home/jstolle/code/cppbeerxels/data/refernece/656189624.805342.jpg";
+  std::filesystem::path bottleCapDirectoryPath =
+      "/home/jstolle/code/cppbeerxels/data/raw/";
 
-  //HoneyCombTiling tiling(400, 500, 25);
-  //tiling.optimalTiling();
-  // tiling.showNodes();
+  CapField field(referenceImageFilePath, bottleCapDirectoryPath);
+  field.runCapShepherd();
+  field.processReference();
+  field.computePlacement();
 }
