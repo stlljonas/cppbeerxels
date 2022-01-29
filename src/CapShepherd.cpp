234f8abdbe7cfd947200930d39a79f68466d7bbd
Jonas Stolle <jonas.stolle@sensirion.com>
2022-01-29 11:34:09 +0100

    Add progress indicator to processCaps

--- src/CapShepherd.cpp
+++ src/CapShepherd.cpp
@@ -10,7 +10,7 @@ void CapShepherd::init() {
   for (const auto &file : std::filesystem::directory_iterator(path)) {
 
     if (file.is_regular_file()) {
-      // std::cout << "Found file " << filenameStr << std::endl;
+      std::cout << "Creating cap from file " << file.path().filename().string() << std::endl;
       std::unique_ptr<Cap> newCap(new Cap(file.path()));
       caps.push_back(std::move(newCap));
     }
@@ -18,8 +18,12 @@ void CapShepherd::init() {
 }
 
 void CapShepherd::processCaps() {
+  std::cout << "Processing Caps:\n";
   for (std::vector<std::unique_ptr<Cap>>::iterator cap = caps.begin();
        cap != caps.end(); ++cap) {
     cap->get()->analyze();
+    std::cout << ".";
+    std::cout.flush();
   }
+  std::cout << " done" << std::endl;
 }
